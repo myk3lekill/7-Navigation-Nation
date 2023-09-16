01a4576deeb81522c81585dc084ec523b89a3d09
me <me@gmail.com>
2023-09-16 11:44:20 +0200

    sixth commit: clean code

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
 
-
+navItems = [nav1, nav2, nav3, nav4, nav5]
 
 function toggleNav() {
     //Toggle: Menu Bars Open/Closed
@@ -16,29 +16,39 @@ function toggleNav() {
     overlay.classList.toggle('overlay-active');
     if(overlay.classList.contains('overlay-active')) {
         //Animate In - Overlay
-        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right'); //clean code
+        overlay.classList.remove('overlay-slide-left') //remove class
+        overlay.classList.add('overlay-slide-right') //add class
         //Animate In: Nav Items
-        nav1.classList.replace('slide-out-1', 'slide-in-1');//clean code
-        nav2.classList.replace('slide-out-2', 'slide-in-2');//clean code
-        nav3.classList.replace('slide-out-3', 'slide-in-3');//clean code
-        nav4.classList.replace('slide-out-4', 'slide-in-4');//clean code
-        nav5.classList.replace('slide-out-5', 'slide-in-5');//clean code
+        nav1.classList.remove('slide-out-1')
+        nav1.classList.add('slide-in-1')
+        nav2.classList.remove('slide-out-2')
+        nav2.classList.add('slide-in-2')
+        nav3.classList.remove('slide-out-3')
+        nav3.classList.add('slide-in-3')
+        nav4.classList.remove('slide-out-4')
+        nav4.classList.add('slide-in-4')
+        nav5.classList.remove('slide-out-5')
+        nav5.classList.add('slide-in-5')
     } else {
         //Animate Out - Overlay
-        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');//clean code
+        overlay.classList.remove('overlay-slide-right') //remove class
+        overlay.classList.add('overlay-slide-left') //add class
         //Animate In: Nav Items
-        nav1.classList.replace('slide-in-1', 'slide-out-1');//clean code
-        nav2.classList.replace('slide-in-2', 'slide-out-2');//clean code
-        nav3.classList.replace('slide-in-3', 'slide-out-3');//clean code
-        nav4.classList.replace('slide-in-4', 'slide-out-4');//clean code
-        nav5.classList.replace('slide-in-5', 'slide-out-5');//clean code
+        nav1.classList.remove('slide-in-1')
+        nav1.classList.add('slide-out-1')
+        nav2.classList.remove('slide-in-2')
+        nav2.classList.add('slide-out-2')
+        nav3.classList.remove('slide-in-3')
+        nav3.classList.add('slide-out-3')
+        nav4.classList.remove('slide-in-4')
+        nav4.classList.add('slide-out-4')
+        nav5.classList.remove('slide-in-5')
+        nav5.classList.add('slide-out-5')
     }
 }
 
 //Event Listeners
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
-nav3.addEventListener('click', toggleNav);
-nav4.addEventListener('click', toggleNav);
-nav5.addEventListener('click', toggleNav);
+navItems.forEach((nav) => { //clean code
+    nav.addEventListener('click', toggleNav)
+});
